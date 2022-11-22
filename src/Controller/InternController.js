72bee6c5a4ae12024e35e8ba1cd638677ceeb5f1
reesheeka <rishikasingh9525@gmail.com>
2022-11-22 13:19:24 +0530

    final project

--- src/Controller/InternController.js
+++ src/Controller/InternController.js
@@ -1,79 +1,67 @@
-const InternModel = require('../Models/InternModel')
-const collegeModel = require('../Models/collegeModel')
+const internModel = require('../Models/InternModel.js');
+const collegeModel = require('../Models/CollegeModel.js');
 
-const { checkName, checkEmail, mobileNum, validValue } = require('../Validator/valid')
+const { checkName, checkEmail, mobileNum, validValue } = require('../Validator/valid.js');
 
 const createIntern = async (req, res) => {
     try {
         const { name, email, mobile, collegeName } = req.body
 
+        if (Object.keys(req.body).length == 0) {
+            return res.status(400).send({ status: false, message: "Please Enter Details In Body😒😒😒" });
+        }
+
         if (!validValue(name)) {
-            return res.status(400).send({ status: false, message: 'Please Enter Name😑😑😑' })
+            return res.status(400).send({ status: false, message: 'Please Enter Name😑😑😑' });
         }
         if (!checkName(name)) {
-            return res.status(400).send({ status: false, message: 'Please Enter A Valid Name😑😑😑' })
+            return res.status(400).send({ status: false, message: 'Please Enter A Valid Name😑😑😑!' });
         }
         if (!validValue(email)) {
-            return res.status(400).send({ status: false, message: 'Please Enter E-mail😑😑😑' })
+            return res.status(400).send({ status: false, message: 'Please Enter Email😑😑😑' });
         }
         if (!checkEmail(email)) {
-            return res.status(400).send({ status: false, message: 'Please Enter A valid E-mail😑😑😑' })
+            return res.status(400).send({ status: false, message: 'Please Enter A valid Email😑😑😑!' });
         }
         if (!validValue(mobile)) {
-            return res.status(400).send({ status: false, message: 'Please Enter Mobile Number😑😑😑 !' })
+            return res.status(400).send({ status: false, message: 'Please Enter Mobile Number😑😑😑' });
         }
         if (!mobileNum(mobile)) {
-            return res.status(400).send({ status: false, message: 'Please Enter A valid Mobile Number 😑😑😑!' })
+            return res.status(400).send({ status: false, message: 'Please Enter A valid Mobile Number 😑😑😑!' });
         }
         if (!validValue(collegeName)) {
-            return res.status(400).send({ status: false, message: 'Please Enter College -Name😑😑😑 !' })
+            return res.status(400).send({ status: false, message: 'Please Enter College Name😑😑😑' });
         }
         if (!checkName(collegeName)) {
-            return res.status(400).send({ status: false, message: 'Please Enter A Valid College Name😑😑😑' })
+            return res.status(400).send({ status: false, message: 'Please Enter A Valid College Name😑😑😑' });
         }
-        const EmailExist = await InternModel.findOne({ email: email })
-        if (EmailExist) return res.status(400).send({ status: false, message: 'Email Already Exist-🤦‍♀️🤦 !' })
 
-        const mobExist = await InternModel.findOne({ mobile: mobile })
-        if (mobExist) return res.status(400).send({ status: false, message: 'Mobile Already Exist-🤦‍♀️🤦 !' })
+        const EmailExist = await internModel.findOne({ email: email })
+        if (EmailExist) return res.status(400).send({ status: false, message: 'Email Already Exist🤦‍♀️🤦!' })
+
+        const mobExist = await internModel.findOne({ mobile: mobile })
+        if (mobExist) return res.status(400).send({ status: false, message: 'Mobile Already Exist🤦‍♀️🤦 !' })
 
         const findCollegeId = await collegeModel.findOne({ name: req.body.collegeName })
+
         if (findCollegeId) {
             req.body.collegeId = findCollegeId._id
-            const internData = await InternModel.create(req.body)
-            res.status(201).send({ status: true, data: internData })
+            const createIntern = await internModel.create(req.body)
+
+            const getIntern = await internModel.findOne(createIntern).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
+
+            return res.status(201).send({ status: true, data: getIntern });
         } else {
-            res.status(400).send({ status: false, msg: 'wrong college name 😒😒😒 ' })
+            return res.status(400).send({ status: false, msg: 'Wrong College Name😒😒😒' });
         }
-
-    } catch (error) {
-        res.status(500).send({ status: false, message: error.message })
     }
-
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message });
+    }
 }
+
+
+
 module.exports.createIntern = createIntern
 
-const getCollegeDetails = async (req, res) => {
-    try {
-        const { collegeName } = req.query
-        if (!validValue(collegeName)) {
-            return res.status(400).send({ status: false, message: 'Please Enter College-Name😑😑😑 !' })
-        }
-        const getCollege = await collegeModel.findOne({ name: collegeName })
-        if (!getCollege) return res.status(400).send({ status: false, message: 'College-Name Does not Exist 😑😑😑!' })
 
-        const getInterns = await InternModel.find({ collegeId: getCollege._id }).select({ name: 1, email: 1, mobile: 1, _id: 1 })
-        const details = {
-            name: getCollege.name,
-            fullName: getCollege.fullName,
-            logoLink: getCollege.logoLink,
-            interns: getInterns
-        };
-
-        res.status(201).send({ status: true, data: details })
-    } catch (error) {
-        res.status(500).send({ status: false, message: error.message })
-    }
-
-}
-module.exports.getCollegeDetails = getCollegeDetails
--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const route = require('./Routes/route')
-const app = express()
-app.use(express.json())
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./Routes/route.js');
+const app = express();
+
+app.use(express.json());
 
 
+mongoose.connect('mongodb+srv://bloggingSite:project123@project-01-group-3.2zpxn0w.mongodb.net/Project-02-Interns', {
+    useNewUrlParser: true
+}).then(() => console.log('MongoDB connected😎😎'))
+    .catch(err => console.log(err.message))
 
-mongoose.connect('mongodb+srv://bloggingSite:project123@project-01-group-3.2zpxn0w.mongodb.net/Project-02-Interns',{
-    useNewUrlParser:true
-}).then( ()=> console.log('MongoDB connected 😎🥰😎'))
-.catch(err=> console.log(err.message))
+app.use('/', route);
 
-app.use('/', route)
-
-app.listen(3000 , function(){
-    console.log('Server Running On Port Num-' , 3000)
-})
+app.listen(3000, function () {
+    console.log('Server Running On Port Number' + 3000)
+});
